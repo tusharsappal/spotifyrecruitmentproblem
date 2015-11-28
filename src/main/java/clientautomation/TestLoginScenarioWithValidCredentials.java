d4154e18f5aa8b4a3a8ad43d7f99be0c813833b5
Tushar Sappal <sappal.tushar@gmail.com>
2015-11-28 15:50:30 +0530

    Adding the logic to detect the drop down and click on logout button

--- src/main/java/clientautomation/TestLoginScenarioWithValidCredentials.java
+++ src/main/java/clientautomation/TestLoginScenarioWithValidCredentials.java
@@ -7,51 +7,37 @@ import org.sikuli.basics.Debug;
 public class TestLoginScenarioWithValidCredentials {
 
 	public static void main(String[] args) throws FindFailed, InterruptedException, AWTException {
-		System.out.println("Current directory is "+ System.getProperty("user.dir"));
+
 		Debug.setDebugLevel(3);
 		Screen screen = new Screen();
 		ImagePath.add(System.getProperty("user.dir")+"/src/main/resources/images");
-		//System.out.println("Path is"+ TestClass.class.getCanonicalName());
-		Pattern userNamePattern = new Pattern("user_name_image_2.png").similar((float)0.7);
+		Pattern userNamePattern = new Pattern("user_name_image.png").similar((float)0.7);
 		Pattern userPasswordPattern = new Pattern("user_password_image.png").similar((float)0.7);
 		Pattern loginButtonPattern = new Pattern("login_button.png").similar((float)0.7);
 		Pattern userNameTopBanner =  new Pattern("user_name_top_banner.png").similar((float)0.7);
 		Pattern userNameDropDown = new Pattern("user_name_drop_down.png").similar((float)0.7);
-		Pattern logOutButton = new Pattern("logout_button.png").similar((float)0.7);
+		Pattern logoutButton = new Pattern("logout_button.png").similar((float)0.7);
 
 		App app = new App("Spotify");
 		app.focus();
 		Thread.sleep(5000);
 		screen.click(userNamePattern);
 
-		// Now navigating to the password field
-
-
 		screen.type(userPasswordPattern,"Accompany123");
-
-		// Now we will be clicking on the Sign in Button
-
-
 
 		screen.click(loginButtonPattern);
 
 		Thread.sleep(5000);
-
-		// First Checking if the user is logged in or not 
 
 		if(screen.exists(userNameTopBanner) != null)
 		{
 			screen.click(userNameDropDown);
 			//screen.wait()
 			screen.wait(userNameDropDown);
-
+			screen.click(logoutButton);
+			Thread.sleep(3000);
 
 		}
-
-		// Adding the code to logout from the Client
-
-
-
 
 		System.out.println(app.isRunning());
 		Thread.sleep(2000);
